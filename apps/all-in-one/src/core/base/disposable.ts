597ca553304780a604fb96d686e53e9f622fe7ad
Optional666 <kexuanzhang@outlook.com>
2024-10-14 23:52:47 +0800

    feat(all-in-on): add dataloader

--- apps/all-in-one/src/core/base/disposable.ts
+++ apps/all-in-one/src/core/base/disposable.ts
@@ -1,3 +1,5 @@
 export interface IDisposable {
   dispose(): void;
 }
+
+export const toDisposable = (dispose: () => void): IDisposable => ({ dispose })
--- apps/all-in-one/src/workbench/dataloader/index.ts
+++ apps/all-in-one/src/workbench/dataloader/index.ts
@@ -2,6 +2,7 @@ import { type ListenerOfEvent } from "@naive/core/base/event";
 import { Database, Table } from "@naive/core/model/interface";
 import { UnimplementsError } from "@naive/core/base/exceptions";
 import { DataLoaderPhase, IDataloader } from "./interface";
+import { toDisposable } from "@naive/core/base/disposable";
 
 export type CreateDataloaderOption = {};
 const createDataloader = (option: CreateDataloaderOption): IDataloader => {
@@ -14,27 +15,21 @@ const createDataloader = (option: CreateDataloaderOption): IDataloader => {
     getPhase: () => phase,
     onPermissionLoaded: (listener) => {
       permissionLoadedLisenters.add(listener);
-      return {
-        dispose: () => {
-          permissionLoadedLisenters.delete(listener);
-        },
-      };
+      return toDisposable(() => {
+        permissionLoadedLisenters.delete(listener);
+      });
     },
     onCurTableLoaded: (listener) => {
       curTableLoadedListeners.add(listener);
-      return {
-        dispose: () => {
-          curTableLoadedListeners.delete(listener);
-        },
-      };
+      return toDisposable(() => {
+        curTableLoadedListeners.delete(listener);
+      });
     },
     onAllTableLoaded: (listener) => {
       allTableLoadedListeners.add(listener);
-      return {
-        dispose: () => {
-          allTableLoadedListeners.delete(listener);
-        },
-      };
+      return toDisposable(() => {
+        allTableLoadedListeners.delete(listener);
+      });
     },
     when: (phase) => {
       throw new UnimplementsError();
