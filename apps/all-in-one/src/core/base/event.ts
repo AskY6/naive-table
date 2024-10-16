1cb9ea82725990543196a167e29321434b40daa0
Optional666 <kexuanzhang@outlook.com>
2024-10-16 23:22:41 +0800

    feat(all-in-on): add dataloader fetcher and dataloader lifecycle

--- apps/all-in-one/src/core/base/event.ts
+++ apps/all-in-one/src/core/base/event.ts
@@ -1,4 +1,16 @@
-import { IDisposable } from "./disposable";
+import { IDisposable, toDisposable } from "./disposable";
 
 export type Event<T> = (listener: (e: T) => any) => IDisposable;
 export type ListenerOfEvent<T> = Parameters<Event<T>>[0]
+
+export const createDisposableListenerKeeper = <T>() => {
+    const set = new Set<ListenerOfEvent<T>>()
+    const addItem = (listener: ListenerOfEvent<T>) => {
+        return toDisposable(() => {
+            set.delete(listener)
+        })
+    }
+    return {
+        add: addItem
+    }
+}
